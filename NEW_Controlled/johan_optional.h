9a7464ff08a67fe76becce1f0c3963b6fd55f469
Johan Sandred <johan.sandred@protonmail.com>
2024-09-20 13:55:07 +0200

    Add optional lab 1 files

---
+++ NEW_Controlled/johan_optional.h
@@ -0,0 +1,13 @@
+/*
+ * johan_optional.h
+ *
+ *  Created on: 20 Sep 2024
+ *      Author: Johan
+ */
+
+#ifndef JOHAN_OPTIONAL_H_
+#define JOHAN_OPTIONAL_H_
+
+int Lab1OptionalJohan(void);
+
+#endif /* JOHAN_OPTIONAL_H_ */

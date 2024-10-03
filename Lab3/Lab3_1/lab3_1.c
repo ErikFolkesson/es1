170c620380110fb178f9b9952275f2910fe611e9
Johan Sandred <johan.sandred@protonmail.com>
2024-10-03 18:09:46 +0200

    Lab 3.1 Use polling and allow printing strings

--- Lab3/Lab3_1/lab3_1.c
+++ Lab3/Lab3_1/lab3_1.c
@@ -1,29 +1,25 @@
 //*****************************************************************************
 #include <stdbool.h>
 #include <stdint.h>
-#include "inc/hw_memmap.h"
+#include <math.h>
+#include <assert.h>
+#include <ctype.h>
+
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/pwm.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
-#include "utils/uartstdio.h"
-#include "inc/tm4c129encpdt.h"
-
-#include <math.h>
 #include "driverlib/rom.h"
 #include "driverlib/rom_map.h"
-
 #include "driverlib/adc.h"
+#include "inc/tm4c129encpdt.h"
+#include "inc/hw_memmap.h"
 
 float pwm_word;
 uint32_t systemClock;
 
-//***********************************************************************
-//                       Configurations
-//***********************************************************************
-// Configure the UART.
 void ConfigureUART(void)
 {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -31,94 +27,109 @@ void ConfigureUART(void)
     GPIOPinConfigure(GPIO_PA0_U0RX);
     GPIOPinConfigure(GPIO_PA1_U0TX);
     GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    uint32_t baudRate = 115200;
+    // 8 data bits, 1 stop bit, and no parity bit.
+    uint32_t uartConfig = UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE
+            | UART_CONFIG_PAR_NONE;
+    // For PIOSC clock, the frequency should be set as 16'000'000
+    uint32_t systemClockFrequency = 16000000;
+    UARTConfigSetExpClk(UART0_BASE, systemClockFrequency, baudRate, uartConfig);
     UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
-    UARTStdioConfig(0, 115200, 16000000);
+
+    UARTEnable(UART0_BASE);
 }
 
-//*****************************************************************************
-//
-// The UART interrupt handler.
-//
-//*****************************************************************************
-void UARTIntHandler(void)
+void uartPuts(const char *str)
 {
-    uint32_t ui32Status;
-
-    // Get the interrupt status.
-    ui32Status = MAP_UARTIntStatus(UART0_BASE, true);
-
-    // Clear the asserted interrupts.
-    MAP_UARTIntClear(UART0_BASE, ui32Status);
-
-    // Loop while there are characters in the receive FIFO.
-    while (MAP_UARTCharsAvail(UART0_BASE))
+    const char *it;
+    for (it = str; *it != '\0'; it++)
     {
-        enum
-        {
-            arrSize = 20
-        };
-        char buf[arrSize];
-        UARTgets(buf, arrSize);
-        UARTprintf("%s\n", buf);
+        UARTCharPut(UART0_BASE, *it);
     }
 }
 
-void UARTIntHandler2(void)
+void moveCursorToStartOfNextLine(void)
 {
-    uint32_t ui32Status;
-
-    //
-    // Get the interrrupt status.
-    //
-    ui32Status = MAP_UARTIntStatus(UART0_BASE, true);
-
-    //
-    // Clear the asserted interrupts.
-    //
-    MAP_UARTIntClear(UART0_BASE, ui32Status);
-
-    //
-    // Loop while there are characters in the receive FIFO.
-    //
-    while (MAP_UARTCharsAvail(UART0_BASE))
-    {
-        //
-        // Read the next character from the UART and write it back to the UART.
-        //
-        MAP_UARTCharPutNonBlocking(UART0_BASE,
-                                   MAP_UARTCharGetNonBlocking(UART0_BASE));
-
-        // MAP_UARTCharGetNonBlocking(UART0_BASE);
-    }
+    uartPuts("\x1B[1E");
+}
+void moveCursorToStartOfPreviousLine(void)
+{
+    uartPuts("\x1B[1F");
 }
 
-//*****************************************************************************
-//
-//                      Main
-//
-//*****************************************************************************
+void eraseCurrentLine(void)
+{
+    uartPuts("\x1B[2K");
+}
+
+void eraseScreen(void)
+{
+    uartPuts("\x1B[2J");
+}
+
+void moveCursorHome(void)
+{
+    uartPuts("\x1B[H");
+}
+
+void moveCursorToStartOfLine(void)
+{
+    uartPuts("\x1B[1G");
+}
+
 int main(void)
 {
     ConfigureUART();
 
-    systemClock = SysCtlClockFreqSet(
-            (SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL
-                    | SYSCTL_CFG_VCO_480),
-            16000);
+    eraseScreen();
+    moveCursorHome();
 
-    // Enable processor interrupts.
-    MAP_IntMasterEnable();
-
-    // Register the interrupt handler function for UART 0.
-    IntRegister(INT_UART0, UARTIntHandler);
-
-    // Enable the UART interrupt.
-    MAP_IntEnable(INT_UART0);
-    MAP_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
-
-    UARTprintf("\033[2JEnter text: ");
+    enum
+    {
+        ENTER = '\r', BACKSPACE = '\b', USER_INPUT_BUF_CAPACITY = 100
+    };
+    char userInputBuf[USER_INPUT_BUF_CAPACITY] = { 0 };
+    int userInputBufSize = 0;
 
     while (1)
     {
+        if (UARTCharsAvail(UART0_BASE))
+        {
+
+            int c = UARTCharGet(UART0_BASE);
+            if (c == ENTER)
+            {
+                moveCursorToStartOfNextLine();
+                eraseCurrentLine();
+
+                uartPuts(userInputBuf);
+                moveCursorToStartOfPreviousLine();
+                eraseCurrentLine();
+
+                userInputBuf[0] = '\0';
+                userInputBufSize = 0;
+            }
+            else if (isprint(c))
+            {
+                assert(userInputBufSize < USER_INPUT_BUF_CAPACITY - 1); // Avoid buffer overrun while keeping the null character in the string.
+                userInputBuf[userInputBufSize++] = c;
+                userInputBuf[userInputBufSize] = '\0';
+                UARTCharPut(UART0_BASE, c);
+            }
+            else if (c == BACKSPACE)
+            {
+                if (userInputBufSize > 0)
+                {
+                    userInputBufSize--;
+                    userInputBuf[userInputBufSize] = '\0';
+                    // Sending a backspace to the terminal only moves the cursor, it doesn't remove any character.
+                    // To erase the character we need to also print a space and then send another backspace.
+                    uartPuts("\b \b");
+                }
+            }
+
+        }
+
     }
 }

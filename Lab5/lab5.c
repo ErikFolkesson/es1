54dc41112fa920eb6b70a355549b967c0d9fa883
Johan Sandred <johan.sandred@protonmail.com>
2024-10-17 21:20:08 +0200

    Lab 5_1: Add structs for task arguments

--- Lab5/lab5.c
+++ Lab5/lab5.c
@@ -3,14 +3,31 @@
 #include "semphr.h"
 
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 #include "drivers/buttons.h"
 #include "drivers/pinout.h"
 #include "driverlib/gpio.h"
 #include "inc/hw_memmap.h"
 
+typedef struct
+{
+    SemaphoreHandle_t *ledMutex; // The mutex for modifying/holding the LED.
+    uint16_t blinkDuration; // How many microseconds the LED should spend in each on/off state.
+    uint8_t ledId; // CLP_D1 .. CLP_D4.
+} TaskBlinkArgs;
+
+typedef struct
+{
+    SemaphoreHandle_t *ledMutex; // The mutex for modifying/holding the LED.
+    SemaphoreHandle_t *buttonSemaphore; // The semaphore which signals a button press.
+    uint8_t ledId; // CLP_D1 .. CLP_D4.
+} TaskHoldArgs;
+
 void vTaskBlink(void *pvParameters)
 {
+    const TaskBlinkArgs *args = pvParameters;
     // Init specified LED
 
     // Synch, maybe
@@ -24,6 +41,7 @@ void vTaskBlink(void *pvParameters)
 
 void vTaskHold(void *pvParameters)
 {
+    const TaskHoldArgs *args = pvParameters;
 // Take unavailable semaphore
 // Lower priority
     while (true)
@@ -55,6 +73,31 @@ int main(void)
     // enable the GPIO pin for digital function.
     GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE,
                           GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+
+    // Construct task arguments as static variables to make sure they outlive the tasks.
+    static TaskBlinkArgs blink1Args;
+    blink1Args = (TaskBlinkArgs ) { .ledMutex = &LED_mutex_1, .blinkDuration =
+                                            1000,
+                                    .ledId = CLP_D1 };
+    static TaskBlinkArgs blink2Args;
+    blink2Args = (TaskBlinkArgs ) { .ledMutex = &LED_mutex_2, .blinkDuration =
+                                            2000,
+                                    .ledId = CLP_D2 };
+    static TaskBlinkArgs blink3Args;
+    blink3Args = (TaskBlinkArgs ) { .ledMutex = NULL, .blinkDuration = 3000,
+                                    .ledId = CLP_D3 };
+    static TaskBlinkArgs blink4Args;
+    blink4Args = (TaskBlinkArgs ) { .ledMutex = NULL, .blinkDuration = 4000,
+                                    .ledId = CLP_D4 };
+
+    static TaskHoldArgs hold1Args;
+    hold1Args = (TaskHoldArgs ) { .ledMutex = &LED_mutex_1, .buttonSemaphore =
+                                          &button_sem_1,
+                                  .ledId = CLP_D1 };
+    static TaskHoldArgs hold2Args;
+    hold2Args = (TaskHoldArgs ) { .ledMutex = &LED_mutex_2, .buttonSemaphore =
+                                          &button_sem_2,
+                                  .ledId = CLP_D2 };
 
     // Create tasks
     // CLP_D1, CLP_D2, CLP_D3, CLP_D4

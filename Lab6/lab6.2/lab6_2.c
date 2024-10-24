c68204947da7247ee7a45e0001ff862af387c4c3
Johan Sandred <johan.sandred@protonmail.com>
2024-10-24 14:00:34 +0200

    Lab 6.2: Finish lab

--- Lab6/lab6.2/lab6_2.c
+++ Lab6/lab6.2/lab6_2.c
@@ -12,6 +12,7 @@
 #include "driverlib/gpio.h"
 #include "inc/hw_memmap.h"
 #include "driverlib/sysctl.h"
+#include "drivers/buttons.h"
 
 #include "driverlib/pin_map.h"
 #include "driverlib/interrupt.h"
@@ -19,6 +20,7 @@
 #include "inc/tm4c129encpdt.h"
 
 SemaphoreHandle_t g_printMutex;
+SemaphoreHandle_t g_statusSemaphore;
 
 void ConfigureUART(void)
 {
@@ -51,10 +53,12 @@ void uartPuts(const char *str)
     }
 }
 
-enum {
+enum
+{
     BUF_CAPACITY = 15
 };
 
+// +1 to make sure we always have a NUL character at the end of the buffer.
 char buf[BUF_CAPACITY + 1];
 int bufSize;
 int totalCharacterCount;
@@ -66,61 +70,125 @@ void clearScreenAndMoveCursorHome(void)
     uartPuts("\x1B[H");
 }
 
-// If the call needs to be thread-safe, the caller is responsible for locking a mutex.
-void printUartData(void) {
-    clearScreenAndMoveCursorHome();
-    uartPuts(buf);
-    // TODO: poll status semaphore.
+// NOTE: We don't handle numbers larger than 99.
+void uartPrintInt(int number)
+{
+    char tenDigit = '0' + number / 10 % 10;
+    char oneDigit = '0' + number % 10;
+    UARTCharPut(UART0_BASE, tenDigit);
+    UARTCharPut(UART0_BASE, oneDigit);
 }
 
-// Rotates te element in the buffer one step to the left, with the first character moving to the last position.
-void rotateLeft(char*buf, int bufSize) {
+// If the call needs to be thread-safe, the caller is responsible for locking a mutex.
+void printUartData(void)
+{
+    clearScreenAndMoveCursorHome();
+    uartPuts(buf);
+
+    if (xSemaphoreTake(g_statusSemaphore, 0))
+    {
+        xSemaphoreGive(g_statusSemaphore);
+        uartPuts("\r\n");
+        uartPrintInt(totalCharacterCount);
+    }
+}
+
+// Rotates the element in the buffer one step to the left, with the first character moving to the last position.
+void rotateLeft(char *buf, int bufSize)
+{
     char tmp = *buf;
-    for (int i = 1; i < bufSize; i++) {
+    for (int i = 1; i < bufSize; i++)
+    {
         buf[i - 1] = buf[i];
     }
     buf[bufSize - 1] = tmp;
 }
 
-void uartTask(void* parameters) {
-    while (true) {
+// Task that receives input, stores it in our 15-character buffer, and echoes it back.
+void uartTask(void *parameters)
+{
+    while (true)
+    {
         int32_t c = UARTCharGet(UART0_BASE);
-        if (!isprint(c)) {
+        // We don't care to print out any non-printable characters.
+        if (!isprint(c))
+        {
             continue;
         }
+
+        // While modifying the buffer, we don't want anyone to print at the same time.
         xSemaphoreTake(g_printMutex, portMAX_DELAY);
 
-        if (bufSize == BUF_CAPACITY) {
+        if (bufSize == BUF_CAPACITY)
+        {
+            // We move all characters in the buffer one step to the left to make space for the new, appended character.
             rotateLeft(buf, bufSize);
             buf[bufSize - 1] = c;
-        } else {
+        }
+        else
+        {
             buf[bufSize] = c;
             bufSize++;
         }
+
+        totalCharacterCount++;
+
         printUartData();
         xSemaphoreGive(g_printMutex);
+    }
+}
+
+// Task that polls the button and handles turning on the status task when it's pressed.
+void buttonTask(void *parameters)
+{
+    while (true)
+    {
+        uint8_t delta;
+        uint8_t state = ButtonsPoll(&delta, 0);
+
+        if (BUTTON_PRESSED(LEFT_BUTTON, state, delta))
+        {
+            // During the ten seconds the status task should be shown, a semaphore is raised.
+            xSemaphoreGive(g_statusSemaphore);
+
+            xSemaphoreTake(g_printMutex, portMAX_DELAY);
+            printUartData();
+            xSemaphoreGive(g_printMutex);
+
+            vTaskDelay(pdMS_TO_TICKS(1000 * 10));
+            // Once ten seconds have passed, we lower the semaphore for the status task.
+            xSemaphoreTake(g_statusSemaphore, portMAX_DELAY);
+
+            // Since the status task should no longer be shown, we make a new call to printUartData();
+            xSemaphoreTake(g_printMutex, portMAX_DELAY);
+            printUartData();
+            xSemaphoreGive(g_printMutex);
+        }
     }
 }
 
 int main(void)
 {
     ConfigureUART();
+    ButtonsInit();
 
     // Set the system clock to the same as in FreeRTOSConfig.h.
     SysCtlClockFreqSet(
     SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480,
                        SYSTEM_CLOCK);
     g_printMutex = xSemaphoreCreateMutex();
+    g_statusSemaphore = xSemaphoreCreateBinary();
 
     clearScreenAndMoveCursorHome();
 
     const uint16_t taskStackDepth = 1000;
     const UBaseType_t priority = tskIDLE_PRIORITY + 1;
 
-    int a = 4;
-
     BaseType_t result;
-    result = xTaskCreate(uartTask, "uarttask", taskStackDepth, &a, priority,
+    result = xTaskCreate(uartTask, "uarttask", taskStackDepth, NULL, priority,
+                         NULL);
+    assert(result == pdPASS);
+    result = xTaskCreate(buttonTask, "buttontask", taskStackDepth, NULL, priority,
                          NULL);
     assert(result == pdPASS);
 
